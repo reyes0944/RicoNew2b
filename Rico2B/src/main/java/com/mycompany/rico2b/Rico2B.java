34f26ce09ce1d46dd30f1adea4b674af488049b2
reyes0944 <ricoreyes033006@gmail.com>
2025-08-06 21:58:11 +0800

    Add files via upload

---
+++ Rico2B/src/main/java/com/mycompany/rico2b/Rico2B.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rico2b;
+
+/**
+ *
+ * @author ricor
+ */
+public class Rico2B {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
